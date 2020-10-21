11bad4ef3383e350002db9857aaa75c7f61c3460
wangdawei <tb6147877@hotmail.com>
2020-10-21 17:51:24 +0100

    assignment passing parameters

---
+++ cpp_programe/cpp_programe/assignment_passing_parameters.cpp
@@ -0,0 +1,14 @@
+////1."100  200"
+////2."100  200"
+////3."104  204"
+////4."a strange number  76"
+//
+//
+//
+//#include <iostream>
+//int main() {
+//	std::cout << sizeof(int) << '\n';
+//	double v1 = 20.0;
+//	double* p1=&v1;
+//	std::cout << sizeof(p1) << '\n';
+//}
