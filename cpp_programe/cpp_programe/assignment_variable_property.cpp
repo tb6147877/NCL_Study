857afa1a5b9d11572b5e8d85fa8e3e761e9fb0a9
wangdawei <tb6147877@hotmail.com>
2020-10-21 18:30:08 +0100

    assignment variable properties

---
+++ cpp_programe/cpp_programe/assignment_variable_property.cpp
@@ -0,0 +1,39 @@
+//# include < iostream >
+//# include < string >
+//
+//using namespace std;
+//
+////1.If a interger is forever equal or greater than 0, I will use unsigned integer.
+//
+//
+//int main() {
+//	//2
+//	char c{ 'a' };
+//	bool b{ false };
+//	double d(100.0);
+//	std::cout << "char: " << c << "\n";
+//	std::cout << "bool: " << b << "\n";
+//	std::cout << "double: " << d << "\n";
+//
+//
+//     cout << " Hello " << "   "<< " World " << "\n";
+//     std::cout << " What is 7 + 2?" << "\n";
+//
+//	 //3.
+//	 /*int x{}, y{};
+//     cin >> x;
+//	 cin >> y;
+//
+//	 std::cout << x + y << '\n';*/
+//
+//	 //4.
+//	 std::string str{};
+//
+//	 std::cout << "please enter your first name\n";
+//	 std::cin >> str;
+//
+//	 std::cout << "hello " << str << "\n";
+//
+//
+//	return 0;
+//}
